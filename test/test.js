fef0d7f4cf8b91741ba8ddc1fbee9319e9dce28f
mark tyneway <mark@purse.io>
2018-08-28 18:41:05 -0700

    test: add circleci test setup

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const { assert } = require('chai');
+
+const { reduceInterface } = require('../lib/reducers');
+const { updateTextField } = require('../lib/actions');
+
+describe('interface reducer', () => {
+  it('should update properly', () => {
+    const field = 'foo';
+    const value = 'bar';
+    const valid = true;
+    const action = updateTextField(field, value, valid);
+
+    const newState = reduceInterface(undefined, action);
+
+    const expected = {
+      textFields: { [field]: { value, valid } }
+    };
+
+    assert.deepEqual(newState, expected);
+  });
+});
