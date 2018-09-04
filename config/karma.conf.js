414b830a55211e8c69d113bae909a0cc68054312
mark tyneway <mark@purse.io>
2018-09-04 16:50:45 -0700

    test: add browser testing

---
+++ config/karma.conf.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+
+// read https key/cert
+const httpsOptions = {};
+try {
+  httpsOptions.cert = fs.readFileSync(`${__dirname}/certs/cert.pem`);
+  httpsOptions.key = fs.readFileSync(`${__dirname}/certs/key.pem`);
+} catch (e) {
+  console.log(e);
+  process.exit(1);
+}
+
+const webpackConfigs = require('./webpack');
+
+module.exports = function(config) {
+  config.set({
+
+    // Project configurations
+    basePath: '..',
+    frameworks: ['mocha'],
+    files: [
+      'test/hardwareWallet.js'
+    ],
+    exclude: [],
+    preprocessors: {
+      'test/hardwareWallet.js': ['webpack']
+    },
+    webpack: webpackConfigs,
+
+    // Karma configs
+    plugins: [
+      require('karma-webpack'),
+      require('karma-mocha'),
+      require('karma-mocha-reporter'),
+      require('karma-chrome-launcher')
+    ],
+
+    // web server configurations
+    // use TLS for u2f
+    port: 9876,
+    protocol: 'https:',
+    httpsServerOptions: httpsOptions,
+
+    // Disable Certificate checks
+    browsers: ['Chrome_NoCerts'],
+    customLaunchers: {
+      Chrome_NoCerts: {
+        base: 'Chrome',
+        flags: ['--ignore-certificate-errors', '--disable-web-security']
+      }
+    },
+
+    // Karma run configs
+    autoWatch: false,
+    singleRun: true,
+    concurrency: 1,
+
+    // Karma reporting
+    reporters: ['mocha'],
+    colors: true,
+    logLevel: config.LOG_INFO
+  });
+};
---
+++ config/webpack.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BASE_PATH = path.join(__dirname, '..');
+const BUILD_PATH = path.join(BASE_PATH, 'dist');
+const LIB_PATH = path.join(BASE_PATH, 'lib');
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    bledger: './lib/index.js'
+  },
+  externals: {
+    react: 'React'
+  },
+  node: {
+    fs: 'empty'
+  },
+  output: {
+    library: 'bwallet',
+    libraryTarget: 'umd',
+    path: BUILD_PATH,
+    filename: '[name].js'
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['-browser.js', '.js', '.json', 'jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: [LIB_PATH],
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react', 'stage-3'],
+        }
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.BROWSER': JSON.stringify(true),
+    }),
+  ],
+};
