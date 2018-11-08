0366f07301eb4a1648a11e88aaca06c2845f162c
Mark Tyneway <mark.tyneway@gmail.com>
2018-11-08 14:45:42 -0800

    feature: create new multisig wallet (#24)
    * deps: update bpanel-ui
    
    * action: create multisig wallet
    
    * mappings: create new multisig wallet
    
    * ux: render follow up page
    
    * feature: create new multisig wallet
    
    * lint: lint-fix
    
    * bugfix: simplify selection
    
    * refactor: use smart wallet selection
    
    * refactor: delete unneeded component
    
    * selector: select fn for wallet type
    
    * temp: progress commit
    
    * deps: bump bpanel-ui
    
    * lint: lint-fix
    
    * deps: bump
    
    * bugfix: fix test
    
    * lint: lint-fix
    
    * bugfix: variable mixup
    
    * bugfix
    
    * components: render multisig ux
    
    * deps: bump version in peerdeps
    
    * action: join multisig wallet
    
    * component: conditionally render new button
    
    * component: join ms wallet
    
    * hack: get it working
    
    * update fn sig
    
    * bugfix: use local Label import
    
    * container: render send/receive
    
    * lint: lint-fix
    
    * action: join wallet tighten assert
    
    * constants: use path builder
    
    * refactor: use path builder
    
    * lint: lint-fix
    
    * actions: getwallets take into account services
    
    * lint: lint-fix
    
    * bugfix: test
    
    * lint: lint-fix
    
    * bugfix: rename to unit

--- test/walletsReducer.js
+++ test/walletsReducer.js
@@ -65,21 +65,33 @@ const {
   selectAccount,
 } = accountActions;
 
+const stateMock = () => ({
+  clients: { currentClient: { services: { wallet: true, multisig: true } } },
+});
+
 describe('wallets reducer', () => {
   it('should get wallets', async () => {
     let state;
     await getWallets()(action => {
       state = reduceWallets(state, action);
-    });
-    const expected = getWalletResponse;
+    }, stateMock);
+
+    // should only return wallets that
+    // are standard since the standard api
+    // is a superset that also will include
+    // the multisig wallets
+    const expected = getWalletResponse.filter(
+      w => !getMultisigResponse.includes(w)
+    );
+
     assert.deepEqual(state[WALLETS_NAMESPACE], expected);
   });
 
   it('should get multisig wallets', async () => {
     let state;
-    await getWallets('multisig')(action => {
+    await getWallets()(action => {
       state = reduceWallets(state, action);
-    });
+    }, stateMock);
     const expected = getMultisigResponse;
     assert.deepEqual(state[MULTISIG_WALLETS_NAMESPACE], expected);
   });
